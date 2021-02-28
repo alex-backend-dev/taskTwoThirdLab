99c3385a966e1e49cd398d6f5448b565c26cf0b3
Alex Ulyanitskiy <adl821440@gmail.com>
2021-02-28 23:10:10 +0300

    Add project files.

---
+++ taskTwoThirdLab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace taskTwoThirdLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите точность: ");
+            double precision = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сумма равна: " + GetSum(precision));
+
+            Console.ReadLine();
+        }
+
+        public static double GetSum(double precision)
+        {
+            double sum = 0;
+            double term;
+
+            for (int n = 0; ; n++)
+            {
+                term = Math.Pow(-1, n) / GetFactorial(n);
+
+                if (Math.Abs(term) > precision)
+                    break;
+
+                sum += term;
+            }
+
+            return sum;
+        }
+
+        public static int GetFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+
+            return n * GetFactorial(n - 1);
+        }
+    }
+}
